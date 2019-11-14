11a568cdfde9cf5e76d5eded4a284316b79ec2ce
Reif <bridgerrhammond@gmail.com>
2019-11-14 14:30:09 -0600

    cleaned up old code, moved to new repo

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+const { PORT, DATABASE_URL } = require('./config');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+});
+
+app.set('db', db);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}...`);
+});
